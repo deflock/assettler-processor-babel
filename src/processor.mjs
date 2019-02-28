4a2ee4b1a3031fb4dd19811e55f3d9e4a9a12d69
deflock <deflocked@yandex.com>
2019-02-28 17:52:35 +0300

    Initial commit

---
+++ src/processor.mjs
@@ -0,0 +1,66 @@
+import nodepath from 'path';
+import {stripExtension} from '@deflock/path';
+import {transformAsync} from '@babel/core';
+import GenericProcessor from '@assettler/core/lib/generic-processor';
+
+/**
+ *
+ */
+export default class Processor extends GenericProcessor {
+    /**
+     * @param {string} destDir
+     * @param {Object} options
+     */
+    constructor(destDir, options = {}) {
+        super(Object.assign({
+            extensions: ['.mjs', '.js'],
+        }, options));
+
+        this.destDir = destDir;
+    }
+
+    /**
+     * @param {Object} file
+     * @param {Object} params
+     * @returns {Promise<void>}
+     */
+    async onInit(file, params) {
+        return this.doTrack(file, params);
+    }
+
+    /**
+     * @param {Object} file
+     * @param {Object} params
+     * @returns {Promise<void>}
+     */
+    async onAdd(file, params) {
+        return this.doTrack(file, params);
+    }
+
+    /**
+     * @param {Object} file
+     * @param {Object} params
+     * @returns {Promise<void>}
+     */
+    async onChange(file, params) {
+        return this.doTrack(file, params);
+    }
+
+    /**
+     * @param {Object} file
+     * @param {Object} params
+     * @returns {Promise<void>}
+     */
+    async doTrack(file, params) {
+        const relativePath = file.path;
+        const basedir = params.basedir || params.cwd;
+
+        const srcPath = nodepath.resolve(basedir, relativePath);
+        const buildPath = stripExtension(nodepath.resolve(this.destDir, relativePath)) + '.js';
+
+        const content = await this.readFile(srcPath, 'utf8');
+        const result = await transformAsync(content, Object.assign({}, this.options.babelConfig || {}));
+
+        await this.writeFile(buildPath, result.code);
+    }
+}
